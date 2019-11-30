7861ca2f10e865e1dee6a9b2c13b20166fc06dc7
Andres ruales <carlosa.ruales@gmail.com>
2019-11-30 08:30:35 -0500

    Jugador tabla 2 - clase 24

--- src/app/player-dialog/player-dialog.component.ts
+++ src/app/player-dialog/player-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Countries, SquadNumber, Player } from '../interfaces/player';
+import { Countries, SquadNumber } from '../interfaces/player';
 import { PlayerService } from '../services/player.service';
 import { TeamService } from '../services/team.service';
 import { take } from 'rxjs/operators';
@@ -11,52 +11,47 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./player-dialog.component.scss']
 })
 export class PlayerDialogComponent implements OnInit {
-
   private team;
-  public countries = Object.keys(Countries).map(key=>({
-    label:key,
-    key: Countries[key]
-  }));
-  public squadNumber = Object.keys(SquadNumber).slice(Object.keys(SquadNumber).length / 2);
-  private teamService:TeamService
-
-  constructor(private playerService:PlayerService) { }
+  public player;
+  public countries = Object.keys(Countries).map(key => ({ label: key, key: Countries[key] }));
+  public squadNumber = Object.keys(SquadNumber)
+    .slice(Object.keys(SquadNumber).length / 2)
+    .map(key => ({
+      label: key,
+      key: SquadNumber[key]
+    }));
+  constructor(private playerService: PlayerService, private teamService: TeamService) {}
 
   ngOnInit() {
     this.teamService
-    .getTeams()
-    .pipe(take(1))
-    .subscribe(teams=>{
-      if(teams.length>0){
-        this.team = teams[0]
-      }
-    });
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        if (teams.length > 0) {
+          this.team = teams[0];
+        }
+      });
   }
-  
-  private newPlayer(playerFormValue){
+
+  private newPlayer(playerFormValue) {
     const key = this.playerService.addPlayer(playerFormValue).key;
-    const playerFormValueKey ={
+    const playerFormValueKey = {
       ...playerFormValue,
       key
-    }
+    };
     const formattedTeam = {
       ...this.team,
-      players:[...(
-        this.team.players ? this.team.player:[]
-      ),playerFormValueKey]
-    }
-
-    this.teamService.editTeam(formattedTeam)
+      players: [...(this.team.players ? this.team.players : []), playerFormValueKey]
+    };
+    this.teamService.editTeam(formattedTeam);
   }
 
-  onSubmit(playerForm:NgForm){
-    const playerFormValue = {... playerForm.value};
-    if( playerForm.valid){
-      playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted
+  onSubmit(playerForm: NgForm) {
+    const playerFormValue = {...playerForm.value};
+    if (playerForm.valid) {
+      playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted;
     }
-
-    this.newPlayer(playerFormValue)
+    this.newPlayer(playerFormValue);
     window.location.replace('#');
   }
-
 }
